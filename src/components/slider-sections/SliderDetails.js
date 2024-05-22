21d3573a2410d84b5403eebbd67f4dcb63182363
Md Shamim <mdshamim723@gmail.com>
2024-05-22 08:22:07 +0600

    need to fixed

---
+++ src/components/slider-sections/SliderDetails.js
@@ -0,0 +1,19 @@
+const SliderDetails = () => {
+    return (
+        <div>
+            <div className="solutions">
+                <p>Solutions</p>
+                <p>We have solutions for all aspects of your business</p>
+            </div>
+            <h3>Front of House</h3>
+            <p>Streamline the operations of your restaurant</p>
+            <ul>
+                <li>Point of Sale (POS)</li>
+                <li>Pos App</li>
+                <li>Waiter App</li>
+            </ul>
+        </div>
+    );
+};
+
+export default SliderDetails;
